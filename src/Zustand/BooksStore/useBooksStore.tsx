b42c1da4d724899abbc9f30df79ef9560ff16450
hindshyouni <hind.shyouni99@gmail.com>
2024-11-28 16:57:11 +0300

    data and action to Ebook section

--- src/Zustand/BooksStore/useBooksStore.tsx
+++ src/Zustand/BooksStore/useBooksStore.tsx
@@ -2,24 +2,55 @@ import { create } from "zustand";
 import { books, Book } from "./BookData";
 
 interface BookStore {
-    getFilteredBooks: () => Partial<Book>[]; // تعريف التابع الذي يعيد البيانات المفلترة
+    getFilteredBooks: () => Partial<Book>[]; // تعريف التابع الذي يعيد الكائنين الأول والثاني
+    getThirdAndFourthBooks: () => Partial<Book>[]; // تعريف التابع الذي يعيد الكائنين الثالث والرابع
+    getFifthToSeventhBooks: () => Partial<Book>[]; // الكائن الخامس والسادس والسابع
   }
-  
   export const useBookStore = create<BookStore>(() => ({
     getFilteredBooks: () => {
       // استخراج أول كائنين من المصفوفة
       const firstTwoBooks = books.slice(0, 2);
   
       // تطبيق الفلترة للحصول على الحقول المطلوبة فقط
-      return firstTwoBooks.map(({ logoTitle,logoDescription, downloadLink,  bookImage, authorRating, totalBooks, }) => ({
+      return firstTwoBooks.map(({ logoTitle,logoDescription, downloadLink ,   bookDescription, bookImage, authorRating, totalBooks, }) => ({
         logoTitle,
         logoDescription,
         downloadLink,
+        bookDescription,
         bookImage,
         totalBooks,
         authorRating,
         
       }));
     },
+
+
+
+  // التابع لجلب الكائن الثالث والرابع
+  getThirdAndFourthBooks: () => {
+    const thirdAndFourthBooks = books.slice(2, 4); // استخراج الكائن الثالث والرابع
+    return thirdAndFourthBooks.map(({ logoTitle, logoDescription, bookImage, bookDescription, downloadLink, publicationDate, category, authorName }) => ({
+      logoTitle,
+      logoDescription,
+      bookImage,
+      bookDescription,
+      downloadLink,
+      publicationDate,
+      category,
+      authorName,
+    }));
+  },
+
+
+   // التابع لجلب الكائن الخامس إلى السابع
+   getFifthToSeventhBooks: () => {
+    const fifthToSeventhBooks = books.slice(4, 7); // استخراج الكائن الخامس إلى السابع
+    return fifthToSeventhBooks.map(({ bookImage, bookDescription, bookTitle}) => ({
+      bookImage,         // صورة الكتاب
+      bookDescription,   // شرح عن نوع الكتاب
+      bookTitle,             // اسم الكتاب
+    }));
+  },
+
   }));
 export default useBookStore;
